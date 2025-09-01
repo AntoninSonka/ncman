c70565dd56ece0b9e98d261e3ed425372fa3da86
AntoninSonka <antonin.sonka@outlook.com>
2025-09-01 12:22:56 +0200

    started to write headers

---
+++ src/ncman.h
@@ -0,0 +1,13 @@
+#ifndef NCMAN_H
+#define NCMAN_H
+
+#include <ncurses.h>
+
+typedef struct Menu Menu;               // holds windows (buttons and previews) and pointers to other Menus/CommandMenus
+                                        // is declared on the heap, so its programmers responsibility to delete
+
+typedef struct CommandMenu CommandMenu; // 
+
+
+
+#endif // NCMAN_H
