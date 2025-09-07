e5de90a42c092f13f064219b3cd3c7d6827a42d0
AntoninSonka <antoninsonka@outlook.com>
2025-09-07 10:38:47 +0200

    almost nothing

--- src/main.c
+++ src/main.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <cjson/cJSON.h>
 #include "ncman.h"
 
 int main(int argc, char* argv[]){
     Menu* menu = (Menu*) malloc(sizeof(Menu));
-    menu = read_json(menu, "../src/data/test.json");
+    menu = read_json_menu(menu, "../src/data/test.json");
     printf("header: %s\n", menu->header_text);
     printf("preview: %s\n", menu->preview_text);
     free(menu->header_text);
